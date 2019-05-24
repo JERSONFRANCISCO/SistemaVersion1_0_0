56324a2d729ac035e639cc2040d39fdf65287a2e
JERSONFRANCISCO <jersonfjl@live.com>
2019-05-23 18:09:44 -0600

    23/05/2018
    utima actualizacion del día

--- app_bd/base de datosd.sql
+++ app_bd/base de datosd.sql
@@ -42,6 +42,7 @@ values(1,'Estados','B','BORRADO','A')
 
 
 
+
 select 
 CaD.CAT_Descripcion,
 Pri.CAT_Descripcion,
@@ -50,9 +51,17 @@ convert(varchar,tic.TIC_Fecha_Vencimiento, 105) as TIC_Fecha_Vencimiento,
 case when dep.DEP_Titulo is null then '' else dep.DEP_Titulo end as Departamento,
 case when usr.USR_Nombre is null then '' else usr.USR_Nombre end as Titulo,
 tic.USR_Usuario_Creacion,
-tic.TIC_Titulo
+tic.TIC_Titulo,
+case when Ven.VEN_Nombre is null then '' else Ven.VEN_Nombre end as VEN_Nombre,
+case when cli.cli_nombre is null then '' else cli.cli_nombre end as cli_nombre,
+case when pro.PRO_Nombre is null then '' else pro.PRO_Nombre end as PRO_Nombre,
+case when tal.TAL_Descripcion is null then '' else tal.TAL_Descripcion end as TAL_Descripcion
 from TICKET tic
 inner join CATALOGO_DETALLE CaD on ( tic.TIC_Estado = CaD.CAT_Contraccion and CaD.CAT_Tabla = 'Estados')
 inner join CATALOGO_DETALLE Pri on ( tic.TIC_Prioridad = Pri.CAT_Contraccion and Pri.CAT_Tabla = 'Prioridades')
 left join DEPARTAMENTOS dep on ( tic.DEP_DEPARTAMENTO =dep.DEP_Departamento)
 left join USUARIOS usr on (tic.USR_Usuario = usr.USR_Usuario)
+LEFT JOIN SABIOTERRA..VEN_VENDEDORES Ven on (  ven.Ven_vendedor COLLATE DATABASE_DEFAULT = tic.ven_vendedor)
+LEFT JOIN SABIOTERRA..CLI_CLIENTES cli on (  cli.CLI_Cliente COLLATE DATABASE_DEFAULT = tic.CLI_Cliente)
+LEFT JOIN SABIOTERRA..PRO_PROYECTOS pro on ( tic.Pro_Proyecto COLLATE DATABASE_DEFAULT = pro.PRO_Proyecto and cli.CLI_Cliente = pro.CLI_Cliente)
+left join SABIOTERRA..ALQ_TALLER tal on (tal.tal_numero =  tic.Tal_Numero )
