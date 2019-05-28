28b0a4cfe664b7dbd5125e485e02e83ae7dd03a4
JERSONFRANCISCO <jersonfjl@live.com>
2019-05-28 10:31:04 -0600

    28/05/2019
    primer avance del día

--- mantenimientoAJAX.php
+++ mantenimientoAJAX.php
@@ -5,20 +5,12 @@ require_once(__CTR_PATH . "ctr_estandar.php");
 $ctr_estandar = new ctr_estandar();
 
 if ($_POST['key']=='cargarProyectoClientes'){
-	
 	$cliente = $_POST['cliente'];
-
-	$ctr = $ctr_estandar->obtener_Proyectos_Cliente($cliente);
-
-	echo $ctr;
+	echo $ctr_estandar->obtener_Proyectos_Cliente($cliente);
 }
 if ($_POST['key']=='cargarOTCliente'){
-	
 	$cliente = $_POST['cliente'];
 	$proyecto = $_POST['proyecto'];
-	$ctr = $ctr_estandar->obtener_OrdenesTrabajo_Cliente($cliente,$proyecto);
-
-	echo $ctr;
+	echo $ctr_estandar->obtener_OrdenesTrabajo_Cliente($cliente,$proyecto);
 }
-
 ?>
